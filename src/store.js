57e0efe503452de9fcda2e6a86e5b025bedc9250
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-09-05 12:18:29 +0700

    setup editor config

--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
 import {createStore, combineReducers} from 'redux';
-
 import placeReducer from './reducers/placeReducer';
 
-const rootReducer = combineReducers({
-  places: placeReducer,
-});
+const rootReducer = combineReducers({places: placeReducer});
+
+const withReduxDevtools =
+  __DEV__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const configureStore = () => {
-  return createStore(rootReducer);
+  return createStore(rootReducer, withReduxDevtools);
 };
 
 export default configureStore;
